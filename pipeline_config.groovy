01389174fdbfbf59d247d45d387803f4f6bb6636
Anton Orlov <anton.orlov@solarwinds.com>
2022-09-13 18:09:41 +0300

    Commit

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -2,3 +2,13 @@ jte {
     allow_scm_jenkinsfile = false
     pipeline_template = "Jenkinsfile"
 }
+stages {
+    continuous_integration {
+        test
+        build
+        publish
+    }
+    continuous_delivery {
+        deploy
+    }
+}
